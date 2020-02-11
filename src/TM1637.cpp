b4ce811f4a615f279786aa6c4727c1d8dceb8cf9
AKJ7 <akj123429@gmail.com>
2020-02-11 18:42:23 +0100

    In-Between Commits
    I am currently programming on my Raspberry Pi. This commit is simply a backup commit, in case something
     happens to my Pi.

--- src/TM1637.cpp
+++ src/TM1637.cpp
@@ -1,5 +1,6 @@
 #include "TM1637.h"
 
+#define TO_UINT8_T(value) static_cast<uint8_t>(value)
 
 TM1637::MI2C::MI2C(uint8_t clkPin, uint8_t dataPin) : clkPin{clkPin}, dataPin{dataPin}
 {
@@ -26,15 +27,15 @@ void TM1637::MI2C::endTransmission() const
 
 uint8_t TM1637::MI2C::send(uint8_t value) const
 {
-    static uint8_t ACK;
+    uint8_t ACK;
     for (uint8_t i = 0; i < 8; i++)
     {
         digitalWrite(clkPin, LOW);
-        value & 0x01 ? digitalWrite(dataPin, HIGH) : digitalWrite(dataPin, LOW);
+        digitalWrite(dataPin, value & 0x01);
         delayMicroseconds(3);
-        value >>= 1;
         digitalWrite(clkPin, HIGH);
         delayMicroseconds(3);
+        value >>= 1;
     }
     digitalWrite(clkPin, LOW);
     digitalWrite(dataPin, HIGH);
@@ -52,7 +53,57 @@ uint8_t TM1637::MI2C::send(uint8_t value) const
     return ACK;
 }
 
-TM1637::TM1637(uint8_t clkPin, uint8_t dataPin) : mI2C(clkPin, dataPin)
+TM1637::Animator::Animator(const uint8_t* input) noexcept
+{
+}
+
+TM1637::Animator* TM1637::Animator::set(const uint8_t* input) noexcept
+{
+    return this;
+}
+
+TM1637::Animator* TM1637::Animator::setOverflow(bool overflow)
+{
+    this->overflow = overflow;
+    return this;
+}
+
+TM1637::Animator* TM1637::Animator::blink(size_t delay_ts)
+{
+    disp->offMode();
+    delay(delay_ts);
+    disp->onMode();
+    return this;
+}
+
+TM1637::Animator* TM1637::Animator::scroll(size_t delay, uint8_t direction_left)
+{
+
+    return this;
+};
+
+TM1637::Animator* TM1637::Animator::fadeOut(size_t delay_ts)
+{
+   uint8_t counter = (TO_UINT8_T(disp->getBrightness()) - 0x88);
+   do 
+   {
+        disp->changeBrightness(--counter);
+        delay(delay_ts);
+   } while (counter != 0);
+   return this;
+};
+
+TM1637::Animator* TM1637::Animator::fadeIn(size_t delay_ts)
+{
+    uint8_t counter = (TO_UINT8_T(disp->getBrightness()) - 0x88);
+    do {
+        disp->changeBrightness(++counter);
+        delay(delay_ts);
+    } while (counter != 8);
+    return this;
+}
+
+TM1637::TM1637(uint8_t clkPin, uint8_t dataPin) : mI2C(clkPin, dataPin), animator(this)
 {
 }
 
@@ -67,6 +118,12 @@ void TM1637::begin()
 {
     init();
 }
+
+void TM1637::update()
+{
+    display(digits);
+}
+
 
 uint8_t TM1637::fetch(uint8_t value) const noexcept
 {
@@ -96,6 +153,7 @@ void TM1637::send_fixed_address(AddressCommand_e addressCommand, uint8_t value)
     mI2C.beginTransmission();
     mI2C.send(static_cast<uint8_t>(DataCommand_e::FIXED_ADDRESS));
     mI2C.endTransmission();
+    mI2C.beginTransmission();
     mI2C.send(static_cast<uint8_t>(addressCommand));
     mI2C.send(value);
     mI2C.endTransmission();
@@ -104,24 +162,20 @@ void TM1637::send_fixed_address(AddressCommand_e addressCommand, uint8_t value)
     mI2C.endTransmission();
 }
 
-void TM1637::display(uint8_t value[4]) const
+void TM1637::display(uint8_t* value) const
 {
     mI2C.beginTransmission();
     mI2C.send(static_cast<uint8_t>(DataCommand_e::AUTOMATIC_ADDRESS_ADDING));
     mI2C.endTransmission();
     mI2C.beginTransmission();
     mI2C.send(static_cast<uint8_t>(AddressCommand_e::C0H));
-    for (uint8_t i = 0; i < TOTAL_DIGITS; i++)
+    for (uint8_t i = TOTAL_DIGITS; i; --i)
         // If colon is true, or bit i of dp is set, then turn on MSb
-        (colon || (dp & (1<<i))) ? mI2C.send(fetch(value[i]) + 0x80) : mI2C.send(fetch(value[i]));
+        (colon || (dp & (1 << i))) ? mI2C.send(fetch(value[i - 1]) + 0x80) : mI2C.send(fetch(value[i - 1]));
     mI2C.endTransmission();
     mI2C.beginTransmission();
     mI2C.send(static_cast<uint8_t>(brightness));
     mI2C.endTransmission();
-}
-
-void TM1637::display(String message, overflow_e overflow) const
-{
 }
 
 void TM1637::changeDisplayControl(DisplayControl_e displayControl) const
@@ -144,7 +198,7 @@ void TM1637::offMode()
 
 void TM1637::onMode()
 {
-    changeDisplayControl(DisplayControl_e::DISPLAY_ON);
+    changeDisplayControl(brightness);
 }
 
 TM1637::DisplayControl_e TM1637::fetchControl(uint8_t value) const noexcept
@@ -188,6 +242,12 @@ void TM1637::setBrightness(uint8_t value) noexcept
 {
     brightness = fetchControl(value);
 }
+
+TM1637::DisplayControl_e TM1637::getBrightness() const noexcept
+{
+    return brightness;
+}
+
 void TM1637::switchColon() noexcept
 {
     colon = !colon;
@@ -198,3 +258,4 @@ void TM1637::setDp(uint8_t value) noexcept
     dp = value;
 }
 
+#undef TO_UINT8_T
--- src/TM1637.h
+++ src/TM1637.h
@@ -3,6 +3,9 @@
 
 #include "Arduino.h"
 
+#define TM1637_LEGACY 0
+#define MAX_OVERFLOW 32
+
 /****************************************************************
   * TODO:
   *     - Add scroll effect (Vertical scroll and horizontal scroll)
@@ -12,9 +15,12 @@
   *
   **************************************************************/
 
-namespace
+
+
+namespace type_traits
 {   
     // Type traits
+    // TODO: Handle const and volatile types
 
     template <typename T, T v>
     struct integral_constant
@@ -31,8 +37,7 @@ namespace
 
     template <typename T>
     struct is_integral_base : false_type
-    {
-    };
+    {};
 
     template <> struct is_integral_base<char> : true_type {};
     template <> struct is_integral_base<short>: true_type {};
@@ -45,46 +50,61 @@ namespace
 
     template <typename T>
     struct is_integral : is_integral_base<T>
-    {
-    };
+    {};
 
     template <typename T> struct is_floating_point_base: false_type {};
-
     template <> struct is_floating_point_base<float>: true_type {};
     template <> struct is_floating_point_base<double>: true_type {};
     template <> struct is_floating_point_base<long double>: true_type {};
-
     template <typename T> 
     struct is_floating_point : is_floating_point_base<T>
-    {
-    };
+    {};
+
+    template <typename T> struct is_string_base: false_type {};
+    template <> struct is_string_base<const char*>: true_type {};
+    template <> struct is_string_base<String>: true_type {};
+
+    template <typename T>
+    struct is_string : is_string_base<T>
+    {};
+
+    template <bool B, class T = void>
+    struct enable_if
+    {};
+
+    template <class T>
+    struct enable_if <true, T> { using type = T; };
+    
+    template <bool B, class T = void>
+    using enable_if_t = typename enable_if<B, T>::type;
 };
 
 
 class TM1637 {
 public:
+    static constexpr uint8_t TOTAL_DIGITS = 4;
     enum class DataCommand_e: uint8_t
     {
-        FIXED_ADDRESS = 0x44,
-        AUTOMATIC_ADDRESS_ADDING = 0x40,
-        READ_KEY_SCAN_DATA = 0x42,
-        WRITE_DATA_TO_DISPLAY_REGISTER = 0x40,
-        NORMAL_MODE = 0x40,
-        TEST_MODE = 0x48
+        FIXED_ADDRESS                   = 0x44,
+        AUTOMATIC_ADDRESS_ADDING        = 0x40,
+        READ_KEY_SCAN_DATA              = 0x42,
+        WRITE_DATA_TO_DISPLAY_REGISTER  = 0x40,
+        NORMAL_MODE                     = 0x40,
+        TEST_MODE                       = 0x48
     };
     enum class AddressCommand_e: uint8_t
     {
-        C0H = 0xC0,
+        C0H                             = 0xC0,
         C1H,
         C2H,
         C3H,
         C4H,
         C5H,
-        C6H  // Ignore Scan
+        C6H                             // Ignore Scan
     };
     enum class DisplayControl_e: uint8_t
     {
-        PULSE_WIDTH_1_16 = 0x88,
+        PULSE_WIDTH_1_16                = 0x88,
         PULSE_WIDTH_2_16,
         PULSE_WIDTH_4_16,
         PULSE_WIDTH_10_16,
@@ -92,20 +112,14 @@ public:
         PULSE_WIDTH_12_16,
         PULSE_WIDTH_13_16,
         PULSE_WIDTH_14_16,
-        DISPLAY_OFF = 0x80,
-        DISPLAY_ON = 0x88
-    };
-
-    enum class overflow_e: uint8_t
-    {
-        ON,
-        OFF
+        DISPLAY_OFF                     = 0x80,
+        DISPLAY_ON                      = 0x88
     };
 
     /**
      *  @class mI2C
      *  @brief Modified I2C that the display supports.
-     *         The communication protocaoll used by the display is similar to the I2C,
+     *         The communication protocoll used by the display is similar to the I2C,
      *         except for the fact, that it doesn't necessite the transmission of the
      *         slave address. The following class handles the low level communication
      *         between the Display and the controller.
@@ -124,83 +138,120 @@ public:
         ~MI2C() = default;
     };
 
+    template <size_t NUM, typename T>
+    class Array
+    {
+    private:
+        T buffer[NUM] = {};
+        friend TM1637;
+    public:
+        Array() = default;
+        Array(const Array& rhs)
+        {
+
+        }
+        Array(Array&& rhs)
+        {
+        }
+        T& operator[](size_t index) noexcept
+        {
+            return buffer[index];
+        }
+        T operator[](size_t index) const noexcept
+        {
+            return buffer[index];
+        }
+        ~Array() = default;
+    };
+    
+    class Animator
+    {
+    private:
+        static constexpr decltype(TOTAL_DIGITS) WIDTH = TOTAL_DIGITS; 
+        bool overflow;
+        const TM1637* disp;
+    public:
+        Animator(const TM1637* disp)
+        {
+            this->disp = disp;
+        };
+        Animator(const uint8_t* input) noexcept;
+        Animator* set(const uint8_t* input);
+        Animator* setOverflow(bool overflow);
+        Animator* blink(size_t delay_ts);
+        Animator* scroll(size_t delay, uint8_t direction_left);
+        Animator* fadeIn(size_t delay);
+        Animator* fadeOut(size_t delay);
+        ~Animator() = default;
+    };
+
 private:
-    static constexpr uint8_t TOTAL_DIGITS = 4;
     void changeDisplayControl(DisplayControl_e displayControl) const;
     DisplayControl_e fetchControl(uint8_t value) const noexcept;
+    friend Animator;
+    TM1637(const TM1637&) = delete;                                     // No copy for you man!
+    TM1637& operator=(const TM1637&) = delete;
+    Array<MAX_OVERFLOW, uint8_t> digits;
 
 protected:
     MI2C mI2C;
+    Animator animator;
     DisplayControl_e brightness = DisplayControl_e::PULSE_WIDTH_4_16;
-    bool colon = false;
-    uint8_t dp = 0x00; // dp LED bit pattern. LSb is leftmost dp.
+    bool colon                           = false;
+    uint8_t dp                           = 0x00;                        // dp LED bit pattern. LSb is leftmost dp.
     uint8_t numbers[10] = {0x3f, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
     uint8_t alphabet[6] = {0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
-    uint8_t minus       = 0x40;
-    uint8_t empty       = 0x00;
+    static constexpr uint8_t minus       = 0x40;
+    static constexpr uint8_t empty       = 0x00;
     virtual uint8_t fetch(uint8_t value) const noexcept;
     
-
-    // Format: [A, B, C, D, E, F, G, DP] 
-    struct singleDigit
-    {
-    private:
-        uint8_t segments;
-        friend TM1637;
-    public:
-        explicit singleDigit(uint8_t segments) noexcept
-        {
-            this->segments = segments;
-        }
-        SingleDigit()
-        {
-            segments = 0x00;
-        };
-        void clear() noexcept
-        {
-            segments = 0x00;
-        }
-        uint8_t get() const noexcept
-        {
-            return segments;
-        }
-    };
-
-    template <typename Category, typename T, typename Distance = uint8_t, typename Pointer = T*, typename Reference = T&>
-    struct Iterator
-    {
-        using iterator_category = Category;
-        using value_type = T;
-        using difference_type = Distance;
-        using pointer = Pointer;
-        using reference = Reference;
-    };
-
-    template <unsigned TOTAL>
-    class DigIterator : Iterator<uint8_t, singleDigit, uint8_t>
-    {
-    private:
-        pointer ptr;
-        unsigned counter = 0;
-    public:
-        explicit DigIterator(pointer ptr) noexcept : ptr{ptr} {}
-        DigIterator& operator++() { }
-        DigIterator operator++(int) {}
-        bool operator==(DigIterator rhs) const noexcept { return ptr == rhs.ptr; }
-        bool operator!=(DigIterator rhs) const noexcept { return ptr != rhs.ptr; }
-        pointer operator*() { return ptr; }
-    };
-
 public:
-
-    TM1637(uint8_t clkPin, uint8_t dataPin);
+    TM1637(uint8_t clkPin, uint8_t dataPin) noexcept;
     void init();
     void begin();
+    void update();
     void display(uint8_t reg_address, uint8_t value) const;
-    void display(uint8_t value[4]) const;
-    void display(String message, overflow_e overflow = overflow_e::ON) const;
-    void display(const char* message) const;
-    void display(uint8_t value);
+    void display(uint8_t* value) const;
+    template <size_t NUM, typename T>
+    void display(const Array<NUM, T>& arr)
+    {
+        display((T*)arr.buffer);
+    }
+    template <typename T>
+    typename type_traits::enable_if<type_traits::is_integral<T>::value, const Animator*>::type display2(T value, bool overflow = true, bool pad = true, uint8_t offset = 0)
+    {
+        bool negativ = (value < 0);
+        if (offset > TOTAL_DIGITS - 1)
+            return 0;
+        if (negativ) {
+            value = -value;
+        }
+        uint8_t expected = overflow ? MAX_OVERFLOW - offset : TOTAL_DIGITS - offset;
+        if (expected == 0)
+            return 0;
+        do {
+           digits[offset++] = value % 10;
+           value /= 10;
+        } while(value && --expected);
+        if (negativ && expected)
+            digits[offset++] = minus;
+        if (pad)
+            while (expected)
+                digits[offset++] = 0;
+        update();
+        return animator.setOverflow(overflow);
+    };
+    template <typename T>
+    typename type_traits::enable_if<type_traits::is_floating_point<T>::value, const Animator*>::type display2(T value)
+    {
+        return &animator;
+    };
+    template <typename T>
+    typename type_traits::enable_if<type_traits::is_string<T>::value, const Animator*>::type display2(T value)
+    {
+
+        return &animator;
+    };
     void send_fixed_address(AddressCommand_e addressCommand, uint8_t value);
     void setBrightness(uint8_t value) noexcept;
     void changeBrightness(uint8_t value) const;
@@ -208,37 +259,21 @@ public:
     void setDp(uint8_t value);
     void offMode();
     void onMode();
+    DisplayControl_e getBrightness() const noexcept;
+#if TM1637_LEGACY
     template <typename T>
     void dispNumber(T value);
-   // DigIterator begin( return DigIterator<TOTAL_DIGITS>());
-   // DigIterator end();
+#endif
     TM1637() = default;
 };
 
+
+#if TM1637_LEGACY
 template <typename T>
 void TM1637::dispNumber(T value)
 {
-    uint8_t digit[TOTAL_DIGITS] {0};
-    unsigned counter = 0;
-    unsigned expected = 4;
-    bool negative = false;
-    if (value < 0 )
-    {
-        value = -value;
-        expected = 3;
-        negative = true;
-    }
-    while (value >= 10 && counter < expected)
-    {
-        digit[TOTAL_DIGITS - counter - 1] = value % 10;
-        value /= 10;
-        counter++;
-    };
-    digit[TOTAL_DIGITS - counter - 1] = value;
-    counter++;
-    if (negative)
-        digit[TOTAL_DIGITS - counter - 1] = minus;
-    display(digit);
+    display(value);
 }
+#endif
 
 #endif // TM1637_H
